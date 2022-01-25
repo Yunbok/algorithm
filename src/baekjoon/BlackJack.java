06624c6dfbde29179674af9918716bd0c63be58e
Yunbok <qodbsqhr@naver.com>
2022-01-25 23:54:32 +0900

    2798 블랙잭 코드 일부 수정

--- src/baekjoon/BlackJack.java
+++ src/baekjoon/BlackJack.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class BlackJack {
     public static void main(String[] args) {
+
+        System.out.println(blackJack());
+    }
+
+    public static int blackJack(){
         Scanner sc = new Scanner(System.in);
 
         int cardCnt = sc.nextInt();
         int maxValue = sc.nextInt();
         int[] cardDeck = new int[cardCnt];
         int result = 0;
-        boolean flag = false;
 
         for ( int i = 0; i < cardCnt; i++) {
             cardDeck[i] = sc.nextInt();
@@ -18,26 +22,17 @@ public class BlackJack {
 
 
         for ( int i = 0; i < cardCnt-2; i++) {
-            for ( int j = i+1;j < cardCnt-1; j++) {
-                for ( int k = j+1; k < cardCnt; k++) {
+            for (int j = i + 1; j < cardCnt - 1; j++) {
+                for (int k = j + 1; k < cardCnt; k++) {
                     int cardSum = cardDeck[i] + cardDeck[j] + cardDeck[k];
-                    if ( cardSum < maxValue) {
-                        result = Math.max(result , cardSum);
-                    } else if (cardSum == maxValue ) {
-                        result = maxValue;
-                        flag = true;
-                        break;
+                    if (cardSum < maxValue) {
+                        result = Math.max(result, cardSum);
+                    } else if (cardSum == maxValue) {
+                        return maxValue;
                     }
-
                 }
-                if (flag) {
-                    break;
-                }
-            }
-            if (flag) {
-                break;
             }
         }
-        System.out.print(result);
+        return result;
     }
 }
