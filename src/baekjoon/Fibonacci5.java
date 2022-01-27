5c70b7ca5c37ecce2796d44359ab772851264732
Yunbok <qodbsqhr@naver.com>
2022-01-27 23:49:06 +0900

    피보나치5 구현

---
+++ src/baekjoon/Fibonacci5.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Fibonacci5 {
+
+    static int[] fibo = new int[21];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        System.out.println(fibonacci(N));
+    }
+
+
+    public static int fibonacci(int index ) {
+        if (index == 0){
+            return fibo[0];
+        } else if( index == 1) {
+            return fibo[1];
+        }
+        fibo[index] = fibonacci(index -1) + fibonacci(index -2);
+
+        return fibo[index];
+    }
+}
