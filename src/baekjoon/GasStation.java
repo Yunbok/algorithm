821c2cf72b03d770da9fef085445b489a7afa415
Yunbok <qodbsqhr@naver.com>
2022-02-03 23:01:52 +0900

    13305 주유소 소스 수정후 100점

--- src/baekjoon/GasStation.java
+++ src/baekjoon/GasStation.java
@@ -9,12 +9,10 @@ public class GasStation {
         Scanner sc = new Scanner(System.in);
 
         int city = sc.nextInt();
-        int[] distance = new int[city-1];
-        int[] gasPrice = new int[city];
-        int passCity = 0;
-        int result = 0;
-        int temp = 0;
-
+        long[] distance = new long[city-1];
+        long[] gasPrice = new long[city];
+        long result = 0;
+        long temp = 0;
 
         for ( int i = 0; i < distance.length; i++){
             distance[i] = sc.nextInt();
@@ -24,21 +22,14 @@ public class GasStation {
         }
 
         temp = gasPrice[0];
-        for ( int i = 1; i < gasPrice.length; i++){
+        result += distance[0] * temp;
+        for ( int i = 1; i < distance.length; i++){
             if (temp <= gasPrice[i]) {
-                passCity++;
+                result += distance[i] * temp;
                 continue;
             }
-            for (int j = passCity; j >= 0; j--) {
-                result += distance[i - j -1] * temp;
-            }
-            passCity = 0;
             temp = gasPrice[i];
-        }
-        if ( passCity != 0) {
-            for (int j = passCity; j > 0; j--) {
-                result += distance[distance.length - j] * temp;
-            }
+            result += distance[i] * temp;
         }
         System.out.println(result);
     }
