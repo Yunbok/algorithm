b910272991cff7ca2324c025f5294fec1b70714a
배윤복 <qodbsqhr@naver.com>
2022-03-02 22:04:44 +0900

    1064번 평행사변형 오류해결

--- src/baekjoon/Parallelogram.java
+++ src/baekjoon/Parallelogram.java
@@ -1,6 +1,5 @@
 package baekjoon;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Parallelogram {
@@ -12,7 +11,6 @@ public class Parallelogram {
         double[] B = new double[2];
         double[] C = new double[2];
 
-
         A[0] = sc.nextDouble();
         A[1] = sc.nextDouble();
         B[0] = sc.nextDouble();
@@ -20,7 +18,9 @@ public class Parallelogram {
         C[0] = sc.nextDouble();
         C[1] = sc.nextDouble();
 
-        if((A[0]-B[0])*(A[1]-C[1])==(A[1]-B[1])*(A[0]-C[0])) {
+        if ( A[0] - B[0] == 0 && A[0] - C[0] == 0 || A[1] - B[1] == 0 && A[1] - C[1] == 0 ) {
+            System.out.println(-1.0);
+        } else if((A[0]-B[0])*(A[1]-C[1])==(A[1]-B[1])*(A[0]-C[0])) {
             System.out.println(-1.0);
         } else {
 
@@ -30,25 +30,12 @@ public class Parallelogram {
 
             double maxVel = AB + AC + BC - Math.min(AB, Math.min(BC,AC));
             double minVel = AB + AC + BC - Math.max(AB, Math.max(BC,AC));
-            System.out.println(2*maxVel - 2*minVel);
+            System.out.println(2*(maxVel - minVel));
 
         }
     }
 
     public static double pythagoras(double[] a , double[] b) {
-        return Math.sqrt(Math.pow(distance(a[0], b[0]),2) + Math.pow(distance(a[1], b[1]),2));
-    }
-
-    public static double distance(double a , double b) {
-        double high = Math.max(a ,b);
-        double low = Math.min(a,b);
-
-        if ( high > 0 && low > 0 ) {
-            return high - low;
-        } else if ( high <= 0 && low <= 0) {
-            return (high + low) * -1;
-        } else {
-            return high + (low * -1);
-        }
+        return Math.sqrt(Math.pow(a[0] - b[0],2) + Math.pow(a[1] - b[1],2));
     }
 }
