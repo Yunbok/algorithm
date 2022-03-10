329e9ac2935d5a90d3be096c9977895999d7fe03
Yunbok <30767280+Yunbok@users.noreply.github.com>
2022-03-10 15:25:19 +0900

    2178번 미로탐색 BFS 방식 풀이
    BFS  큐에서 poll 할 때 방문 체크를 하게 되면 같은 노드를 여러번 방문하게 되어 메모리 초과 오류가 발생함.

---
+++ src/baekjoon/MazeExploration.java
@@ -0,0 +1,62 @@
+package algorithm.lotto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class MazeExploration {
+	static int N;
+	static int M;
+	static boolean[][] check;
+	static int[][] maze;
+	static int[] xArr = {-1, 1, 0, 0};
+	static int[] yArr = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		maze = new int[N][M];
+		check = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			String row = bf.readLine();
+			for (int j = 0; j < M; j++) {
+				maze[i][j] = row.charAt(j) - '0';
+			}
+		}
+		
+		check[0][0] = true;
+		bfs(0,0);
+
+	}
+	
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<>();
+		queue.add(new int[] {x,y});
+		
+		while(!queue.isEmpty()){
+			
+			int[] targetNode = queue.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = targetNode[0] + xArr[i];
+				int ny = targetNode[1] + yArr[i];
+				
+				if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+					if (maze[nx][ny] > 0 && !check[nx][ny]) {
+						maze[nx][ny] = maze[targetNode[0]][targetNode[1]] + 1;
+						check[nx][ny] = true;
+						queue.add(new int[] {nx,ny});
+					}
+				}
+			}
+		}
+		System.out.println(maze[N-1][M-1]);
+	}
+}
