913527bed2e7efdd1f6c25e029601a1273e6a4ef
배윤복 <qodbsqhr@naver.com>
2022-06-30 21:46:54 +0900

    슬라이딩 윈도우 DP 예제

---
+++ src/baekjoon/GoDown.java
@@ -0,0 +1,83 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class GoDown {
+    public static String[][] stairs;
+    public static int N;
+    public static int maxResult = -1;
+    public static int minResult = 999999;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        stairs = new String[N][3];
+
+        int[] maxDp = new int[3];
+        int[] minDp = new int[3];
+
+        for (int i = 0; i < N; i++) {
+            String[] st = br.readLine().split(" ");
+
+            int x1 = Integer.parseInt(st[0]);
+            int x2 = Integer.parseInt(st[1]);
+            int x3 = Integer.parseInt(st[2]);
+
+            if (i == 0) {
+                maxDp[0] = minDp[0] = x1;
+                maxDp[1] = minDp[1] = x2;
+                maxDp[2] = minDp[2] = x3;
+            } else {
+                // 최댓값을 구하는 dp 배열
+                int beforeMaxDp_0 = maxDp[0], beforeMaxDp_2 = maxDp[2];
+                maxDp[0] = Math.max(maxDp[0], maxDp[1]) + x1;
+                maxDp[2] = Math.max(maxDp[1], maxDp[2]) + x3;
+                maxDp[1] = Math.max(Math.max(beforeMaxDp_0, maxDp[1]), beforeMaxDp_2) + x2;
+                // 최솟값을 구하는 dp 배열
+                int beforeMinDp_0 = minDp[0], beforeMinDp_2 = minDp[2];
+                minDp[0] = Math.min(minDp[0], minDp[1]) + x1;
+                minDp[2] = Math.min(minDp[1], minDp[2]) + x3;
+                minDp[1] = Math.min(Math.min(beforeMinDp_0, minDp[1]), beforeMinDp_2) + x2;
+            }
+        }
+//        dfs(0, 1, 0, true);
+//        dfs(0, 2, 0, true);
+//        dfs(0, 0, 0, true);
+//
+//        dfs(0, 0, 0, false);
+//        dfs(0, 1, 0, false);
+//        dfs(0, 2, 0, false);
+
+        System.out.println(Math.max(maxDp[0], Math.max(maxDp[1], maxDp[2])) + " " + Math.min(minDp[0], Math.min(minDp[1], minDp[2])));
+    }
+
+    private static void dfs(int i, int index, int result, boolean minFlag) {
+
+        if (i == N) {
+            if (minFlag) {
+                minResult = Math.min(minResult, result);
+            } else {
+                maxResult = Math.max(maxResult, result);
+            }
+            return;
+        }
+
+        if (index == 0) {
+            dfs(i+1, 0, result + Integer.parseInt(stairs[i][index]), minFlag);
+            dfs(i+1, 1, result + Integer.parseInt(stairs[i][index]), minFlag);
+
+        } else if (index == 1) {
+            dfs(i+1, 0, result + Integer.parseInt(stairs[i][index]), minFlag);
+            dfs(i+1, 1, result + Integer.parseInt(stairs[i][index]), minFlag);
+            dfs(i+1, 2, result + Integer.parseInt(stairs[i][index]), minFlag);
+
+        } else if (index == 2) {
+            dfs(i+1, 1, result + Integer.parseInt(stairs[i][index]), minFlag);
+            dfs(i+1, 2, result + Integer.parseInt(stairs[i][index]), minFlag);
+        }
+    }
+}
